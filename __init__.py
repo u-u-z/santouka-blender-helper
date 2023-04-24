ee9a61e15cc1e7a6ff91c054d842a60a5f4da923
Remi IO <i@remi.email>
2023-04-24 14:35:29 +0800

    style: format code

--- __init__.py
+++ __init__.py
@@ -9,27 +9,33 @@ bl_info = {
     "category": "Object",
 }
 
-class CustomProperties(bpy.types.PropertyGroup):    
+
+class CustomProperties(bpy.types.PropertyGroup):
     thinning_float: bpy.props.FloatProperty(name="减薄/增厚量")
+
 
 class MessageBox(bpy.types.Operator):
     icon: bpy.props.StringProperty(name="Icon", default="ERROR")
     message: bpy.props.StringProperty(name="Message", default="")
-    
+
     bl_idname = "message.message_box"
     bl_label = "提示"
-    
+
     def execute(self, context):
         return {'FINISHED'}
+
     def invoke(self, context, event):
         return context.window_manager.invoke_props_dialog(self, width=200)
+
     def draw(self, context):
         layout = self.layout
         col = layout.column(align=True)
-        col.label(text=self.message, icon=self.icon) # 
-        
+        col.label(text=self.message, icon=self.icon)
+
+
 def show_message_box(message):
     bpy.ops.message.message_box('INVOKE_DEFAULT', message=message)
+
 
 class OBJECT_OT_add_cube(bpy.types.Operator):
     bl_idname = "object.add_cube"
@@ -41,21 +47,27 @@ class OBJECT_OT_add_cube(bpy.types.Operator):
         show_message_box("Added a cube! ")
         bpy.ops.mesh.primitive_cube_add()
         return {'FINISHED'}
+
+
 class OBJECT_OT_move_to_zero(bpy.types.Operator):
     bl_idname = "object.move_to_zero"
     bl_label = "移动原点"
     bl_options = {'REGISTER', 'UNDO'}
-    
+
     def execute(self, context):
-        selected_objects = context.selected_objects
-        if len(selected_objects) == 0:
+        if len(context.selected_objects) == 0:
             show_message_box("没有选择任何对象")
             return {'FINISHED'}
         else:
-            for obj in selected_objects:
-                bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY', center='BOUNDS')
+            for obj in context.selected_objects:
+                # first active (select) this object
+                bpy.context.view_layer.objects.active = obj
+                bpy.ops.object.origin_set(
+                    type='ORIGIN_GEOMETRY', center='BOUNDS')
                 obj.location = (0, 0, 0)
                 return {'FINISHED'}
+
+
 class OBJECT_OT_undo_last(bpy.types.Operator):
     bl_idname = "object.undo_last"
     bl_label = "Undo Lasts"
@@ -69,7 +81,7 @@ class OBJECT_OT_undo_last(bpy.types.Operator):
 class ThinningObject(bpy.types.Operator):
     bl_idname = "object.thinning_object"
     bl_label = "减小/增加 模型厚度（双平行面）"
-    
+
     def execute(self, context):
         selected_objects = context.selected_objects
         if len(selected_objects) == 0:
@@ -84,19 +96,20 @@ class ThinningObject(bpy.types.Operator):
                 bpy.ops.mesh.select_all(action='SELECT')
                 bpy.ops.mesh.select_mode(type="FACE")
                 bpy.ops.transform.shrink_fatten(
-                    value=thinning_float, 
-                    use_even_offset=False, 
-                    mirror=True, 
-                    use_proportional_edit=True, 
-                    proportional_edit_falloff='SMOOTH', 
-                    proportional_size=0.909091, 
-                    use_proportional_connected=False, 
-                    use_proportional_projected=False, 
+                    value=thinning_float,
+                    use_even_offset=False,
+                    mirror=True,
+                    use_proportional_edit=True,
+                    proportional_edit_falloff='SMOOTH',
+                    proportional_size=0.909091,
+                    use_proportional_connected=False,
+                    use_proportional_projected=False,
                     snap=False,
                     release_confirm=True
                 )
                 bpy.ops.object.mode_set(mode='OBJECT')
         return {'FINISHED'}
+
 
 class CreateObjectsProjectionToZ(bpy.types.Operator):
     bl_idname = "object.create_object_projection"
@@ -111,7 +124,8 @@ class CreateObjectsProjectionToZ(bpy.types.Operator):
             for obj in selected_objects:
                 bpy.context.view_layer.objects.active = obj
                 mesh_data = bpy.data.meshes.new("projection")
-                projection_object = bpy.data.objects.new("projection", mesh_data)
+                projection_object = bpy.data.objects.new(
+                    "projection", mesh_data)
                 scene = bpy.context.scene
                 scene.collection.objects.link(projection_object)
                 bm = bmesh.new()
@@ -127,13 +141,14 @@ class CreateObjectsProjectionToZ(bpy.types.Operator):
                 bm.to_mesh(mesh_data)
                 bm.free()
 
+
 class SimpleAddonPanel(bpy.types.Panel):
     bl_label = "山头火工具箱"
     bl_idname = "OBJECT_PT_simple_addon"
     bl_category = "山头火工具箱"
-    
-    bl_space_type = 'PROPERTIES' # 'VIEW_3D'
-    bl_region_type = 'WINDOW' # 'UI'
+
+    bl_space_type = 'PROPERTIES'  # 'VIEW_3D'
+    bl_region_type = 'WINDOW'  # 'UI'
     bl_context = 'scene'
 
     def draw(self, context):
@@ -149,11 +164,13 @@ class SimpleAddonPanel(bpy.types.Panel):
         col.label(text="减薄/增厚(正增负减)", icon='WORLD_DATA')
         custom_props = context.scene.custom_props
         col.prop(custom_props, "thinning_float")
-        col.operator("object.thinning_object")  
+        col.operator("object.thinning_object")
+
 
 def register():
     bpy.utils.register_class(CustomProperties)
-    bpy.types.Scene.custom_props = bpy.props.PointerProperty(type=CustomProperties)
+    bpy.types.Scene.custom_props = bpy.props.PointerProperty(
+        type=CustomProperties)
     bpy.utils.register_class(MessageBox)
     bpy.utils.register_class(OBJECT_OT_add_cube)
     bpy.utils.register_class(OBJECT_OT_undo_last)
@@ -161,6 +178,7 @@ def register():
     bpy.utils.register_class(ThinningObject)
     bpy.utils.register_class(SimpleAddonPanel)
     bpy.utils.register_class(CreateObjectsProjectionToZ)
+
 
 def unregister():
     bpy.utils.unregister_class(MessageBox)
@@ -172,7 +190,8 @@ def unregister():
     del bpy.types.Scene.custom_props
     bpy.utils.unregister_class(CustomProperties)
     bpy.utils.unregister_class(CreateObjectsProjectionToZ)
-                               
+
+
 if __name__ == "__main__":
     register()
-    #unregister() #for
+    # unregister() #for
