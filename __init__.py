dc2447ab41ecae09e9d9bdca7e4a695c3ea345c0
Remi IO <i@remi.email>
2023-04-27 16:17:38 +0800

    feat: add bottom generator object operation class (#1)

--- __init__.py
+++ __init__.py
@@ -140,6 +140,99 @@ class CreateObjectsProjectionToZZero(bpy.types.Operator):
                 bm.free()
 
 
+class OBJECT_PT_SantoukaBusinessMeshBottom(bpy.types.Operator):
+    bl_idname = "object.OBJECT_PT_SantoukaBusinessMeshBottom"
+    bl_label = "创建底部（用于吸塑）"
+    bl_description = "Create bottom mesh for vacuum forming"
+
+    def execute(self, context):
+        # get method from dependencies
+        get_bounds = stk_object_print3d_utils.operators.get_bounds
+        create_tmp_plane = stk_object_print3d_utils.operators.create_tmp_plane
+        scale_object = stk_object_print3d_utils.operators.scale_object_110_non_standard
+        clean_after_shrinkwraped = stk_object_print3d_utils.operators.clean_useless_verts_and_faces_after_shrinkwraped
+
+        # get method for modifiers
+        remesh_direct = modifiers.remesh_direct
+        solidify_direct = modifiers.solidify_direct
+        shrinkwrap_project_direct = modifiers.shrinkwrap_project_direct
+
+        # business logic
+        # selected_objects will same as "target_objects"
+        selected_objects = context.selected_objects
+
+        for selected_object in selected_objects:
+
+            # get bounds of selected_object
+            # for caculation plane size & postion for deploy
+            selected_object_bounds = get_bounds(selected_object)
+
+            # create plane by selected_object_bounds
+            tmp_plane_x = selected_object_bounds[1] - selected_object_bounds[0]
+            tmp_plane_y = selected_object_bounds[3] - selected_object_bounds[2]
+            tmp_plane_top_z = selected_object_bounds[5]
+
+            # if x,y is too small, then report warning
+            if tmp_plane_x < 0.1 or tmp_plane_y < 0.1:
+                self.report({'WARNING'}, "选择对象的宽度或高度过小")
+                return {'FINISHED'}
+
+            try:
+                # create tmp_plane and scale to selected_object size
+                # tmp_plane_not_scaled: tmp_plane without scale
+                # tmp_plane: tmp_plane after scale
+                # scale / resize plane to selected_object size
+                # will use tmp_plane for remesh & shrinkwrap
+                # plane need enough vertices & faces for shrinkwrap
+
+                tmp_plane_not_scaled = create_tmp_plane(bpy)
+                tmp_plane_object = scale_object(bpy, tmp_plane_not_scaled, {
+                    'x': tmp_plane_x,
+                    'y': tmp_plane_y,
+                })
+
+                # move plane_object to top of selected_object
+                tmp_plane_object.location.z = tmp_plane_top_z + 5
+
+                # remesh: tmp_plane -> tmp_plane_remeshed
+                # for next shrinkwrap need more vertices & faces
+                # TODO: remesh need more options (size) for user panel
+                tmp_plane_object_remeshed = remesh_direct(
+                    bpy,
+                    tmp_plane_object
+                )
+
+                # shrinkwrap: tmp_plane_remeshed -> selected_object
+                # made tmp_plane_remeshed fit selected_object(target_object)
+                # TODO: shrinkwrap need more options for user panel
+                tmp_plane_shrinkwraped_tuple = shrinkwrap_project_direct(
+                    bpy, tmp_plane_object_remeshed, selected_object, )
+                tmp_bottom_mesh = tmp_plane_shrinkwraped_tuple[0]
+                # now tmp_bottom_mesh project on to the selected_object
+
+                # move tmp_bottom_mesh mesh useless vertices to z = 0, and clean
+                # useless vertices: the part of
+                #   not shrinkwrap project on the selected_object part
+                stash_tmp_bottom_object = clean_after_shrinkwraped(bpy, tmp_bottom_mesh)
+                # this stash_bottom_object not solidify yet
+
+                # solidify: add solid stash_tmp_bottom_object
+                # TODO: solidify need tickness options for user panel
+                #   default tickness is 0.7mm * 2
+                solidified_object = solidify_direct(bpy, stash_tmp_bottom_object)
+                # cuz it maybe have the bad faces, so need remesh again
+
+                # remesh: solidified_object -> final_object
+                # TODO: remesh need more options (size) for user panel
+                final_object = remesh_direct(bpy, solidified_object)
+
+            except:
+                self.report({'ERROR'}, "底部 mesh 添加失败")
+                return {'FINISHED'}
+
+        return {'FINISHED'}
+
+
 class SantoukaToolBoxPanel(bpy.types.Panel):
     bl_label = "山头火工具箱"
     bl_idname = "OBJECT_PT_simple_addon"
