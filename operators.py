0ebe22a8677fde1d6e7c322c59b8c487a0cddb9d
Remi IO <i@remi.email>
2023-04-27 16:07:03 +0800

    feat: add clean_useless_verts_and_faces_after_shrinkwraped method in operators(#1)

--- operators.py
+++ operators.py
@@ -923,3 +923,34 @@ def get_bounds(obj: bpy.types.Object) -> Tuple[float, float, float, float, float
     min_z = min(world_bounds, key=lambda b: b.z).z
     max_z = max(world_bounds, key=lambda b: b.z).z
     return min_x, max_x, min_y, max_y, min_z, max_z
+
+
+def clean_useless_verts_and_faces_after_shrinkwraped(
+        blender_py: bpy,
+        target_object: bpy.types.Object,
+        height_threshold: float
+) -> bpy.types.Object:
+
+    blender_py.ops.object.select_all(action='DESELECT')
+    target_object.select_set(True)
+    bpy.context.view_layer.objects.active = target_object
+    bpy.ops.object.mode_set(mode='EDIT')
+    target_object_mesh = bmesh.from_edit_mesh(target_object.data)
+    world_matrix = target_object.matrix_world
+
+    for v in target_object_mesh.verts:
+        world_co = world_matrix @ v.co
+        if world_co.z > height_threshold:
+            world_co.z = -1
+            v.co = world_matrix.inverted() @ world_co
+
+    for f in target_object_mesh.faces:
+        face_center_world = world_matrix @ f.calc_center_median()
+        if face_center_world.z < 0:
+            f.select_set(True)
+
+    bpy.ops.mesh.delete(type='FACE')
+    bmesh.update_edit_mesh(target_object_mesh.data)
+    bpy.ops.object.mode_set(mode='OBJECT')
+
+    return target_object_mesh
